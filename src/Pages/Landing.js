02c9476af6497d6b187543f251556bb161c0dcd5
Sean <mac@Seans-MBP.hitronhub.home>
2020-04-06 21:09:41 -0500

    fixed drawer

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -18,14 +18,14 @@ export default (props) => {
   const [pageFour, setPageFour] = useState({paused: true, reverse: false, open: false})
 
   return(
-    <div 
+    <div
       style={{
         position: 'relative'
       }}
     >
-      {screenSize.large && 
+      {screenSize.large &&
         <>
-          <Button 
+          <Button
             style={{
               position: 'absolute',
               zIndex: '999',
@@ -33,7 +33,7 @@ export default (props) => {
               top: '7px',
               color: 'white',
               fontSize: '1.5em'
-            }} 
+            }}
             type="link"
             onClick={() => {
               setDrawerOpen(!drawerOpen)
@@ -42,38 +42,39 @@ export default (props) => {
             <Icon type="menu" />
           </Button>
           <Drawer
-            title="Menu"
             placement="right"
             closable={false}
             onClose={() => setDrawerOpen(!drawerOpen)}
             visible={drawerOpen}
+            width={'160px'}
           >
-            <div 
+            <div
               style={{
-                position: 'absolute',
                 zIndex: '999',
-                right: '10px',
-                top: '10px',
                 color: 'white',
-                fontSize: '1.5em'
+                fontSize: '1.5em',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-end',
               }}
             >
-              <Button 
+              <Button
                 type="link"
                 style={{
                   fontSize: '1.5em',
-                  padding: 0
+                  padding: 0,
+                  margin: '0 0 10px 0'
                 }}
                 onClick={() => {
                   if(!pageOne.open) {
-                    let delay = 
+                    let delay =
                       pageThree.open ?
-                      200 : 
+                      200 :
                       pageFour.open ?
                       300 :
                       0;
                     setTimeout(() => {
-                      setPageOne({paused: false, reverse: true, open: true}) 
+                      setPageOne({paused: false, reverse: true, open: true})
                     }, delay)
                     setTimeout(() => {
                       setPageTwo({paused: false, reverse: true, open: false})
@@ -91,20 +92,21 @@ export default (props) => {
                 About
               </Button>
 
-              <Button 
-                type="link" 
+              <Button
+                type="link"
                 style={{
                   fontSize: '1.5em',
-                  padding: 0
+                  padding: 0,
+                  margin: '0 0 10px 0'
                 }}
                 onClick={() => {
                   if(!pageTwo.open) {
-                      let delay = 
-                        pageFour.open ? 
-                        300 : 
+                      let delay =
+                        pageFour.open ?
+                        300 :
                         0;
                       setTimeout(() => {
-                        setPageTwo({paused: false, reverse: true, open: true}) 
+                        setPageTwo({paused: false, reverse: true, open: true})
                       }, delay)
                       setTimeout(() => {
                         setPageOne({paused: false, reverse: false, open: false})
@@ -122,17 +124,18 @@ export default (props) => {
                 Projects
               </Button>
 
-              <Button 
-                type="link" 
+              <Button
+                type="link"
                 style={{
                   fontSize: '1.5em',
-                  padding: 0
+                  padding: 0,
+                  margin: '0 0 10px 0'
                 }}
                 onClick={() => {
                   if(!pageThree.open){
-                    let delay = 
-                        pageOne.open ? 
-                        300 : 
+                    let delay =
+                        pageOne.open ?
+                        300 :
                         0;
                       setTimeout(() => {
                         setPageThree({paused: false, reverse: true, open: true})
@@ -157,13 +160,14 @@ export default (props) => {
                 type="link"
                 style={{
                   fontSize: '1.5em',
-                  padding: 0
+                  padding: 0,
+                  margin: '0 0 10px 0'
                 }}
                 onClick={() => {
                   if(!pageFour.open){
-                    let delay = 
-                        pageOne.open ? 
-                        450 : 
+                    let delay =
+                        pageOne.open ?
+                        450 :
                         pageTwo.open ?
                         300 :
                         0;
@@ -189,7 +193,7 @@ export default (props) => {
           </Drawer>
         </>
       }
-      <PageTemplate 
+      <PageTemplate
         left={210}
         top={0}
         zIndex={4}
@@ -205,7 +209,7 @@ export default (props) => {
         }}
         page={<Contact />}
       />
-      <PageTemplate 
+      <PageTemplate
         left={140}
         top={33}
         zIndex={2}
@@ -221,15 +225,15 @@ export default (props) => {
         }}
         page={<Resume />}
       />
-      <PageTemplate 
+      <PageTemplate
         left={70}
         top={66}
         zIndex={3}
         animation={pageTwo}
         title="PROJECTS"
-        onMouseEnter={()=>{ 
+        onMouseEnter={()=>{
           if(!pageTwo.open) {
-            setPageTwo({paused: false, reverse: true, open: true}) 
+            setPageTwo({paused: false, reverse: true, open: true})
             setPageOne({paused: false, reverse: false, open: false})
             setPageThree({paused: false, reverse: true, open: false})
             setPageFour({paused: false, reverse: true, open: false})
@@ -237,15 +241,15 @@ export default (props) => {
         }}
         page={<Portfolio />}
       />
-      <PageTemplate 
+      <PageTemplate
         left={0}
         top={99}
         zIndex={4}
         animation={pageOne}
         title="ABOUT"
-        onMouseEnter={()=>{ 
+        onMouseEnter={()=>{
           if(!pageOne.open) {
-            setPageOne({paused: false, reverse: true, open: true}) 
+            setPageOne({paused: false, reverse: true, open: true})
             setPageTwo({paused: false, reverse: true, open: false})
             setPageThree({paused: false, reverse: true, open: false})
             setPageFour({paused: false, reverse: true, open: false})
